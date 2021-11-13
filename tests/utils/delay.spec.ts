26c00cbb282c61be3519015e90a6f5b6fc213b34
Alan North <alanscodelog@gmail.com>
2021-11-12 22:12:50 -0300

    fix: fixed async tests

--- tests/utils/delay.spec.ts
+++ tests/utils/delay.spec.ts
@@ -4,20 +4,18 @@ import { expect } from "@tests/chai"
 
 
 describe(testName(), () => {
-	it("works with default delay", async done => {
+	it("works with default delay", async () => {
 		const start = Date.now()
 		await delay()
 		const total = Date.now() - start
 		expect(total).to.be.at.least(900)
 		expect(total).to.be.at.most(1100)
-		done()
 	})
-	it("missing tests", async done => {
+	it("missing tests", async () => {
 		const start = Date.now()
 		await delay(2000)
 		const total = Date.now() - start
 		expect(total).to.be.at.least(1900)
 		expect(total).to.be.at.most(2100)
-		done()
 	})
 })
