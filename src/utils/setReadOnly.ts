191bcff3cf0d3b5d44e260078b13d97da4d7358f
Alan North <alanscodelog@gmail.com>
2021-12-06 22:26:17 -0300

    feat: added setReadOnly function

---
+++ src/utils/setReadOnly.ts
@@ -0,0 +1,10 @@
+import type { Mutable } from "@/types"
+
+/**
+ * Allows setting a readonly property of an object with type checking on the value.
+ *
+ * Useful for setting readonly properties within classes when they should only be readonly to the user.
+ */
+export function setReadOnly<T, TKey extends keyof T, TMutable extends Mutable<T>>(self: T, key: TKey, value: TMutable[TKey]): void {
+	self[key] = value as any
+}
