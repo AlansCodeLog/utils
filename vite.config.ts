d92b622c9565b89d1c6afbb5d810e71326c4aa8a
Alan North <alanscodelog@gmail.com>
2023-07-08 23:33:21 -0300

    fix: removed babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { babel } from "@rollup/plugin-babel"
 import glob from "fast-glob"
 import { builtinModules } from "module"
 import path from "path"
@@ -22,19 +21,6 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => defineConfig({
 	plugins: [
-		babel({
-			babelHelpers: "runtime",
-			extensions: [".js", ".mjs", "ts"],
-			presets: [
-				["@babel/preset-env", {
-					modules: false,
-					useBuiltIns: "usage",
-					debug: true,
-					corejs: packageJson.dependencies["core-js"].slice(1, 4),
-				}],
-			],
-			plugins: ["@babel/plugin-transform-runtime"],
-		}),
 		tsconfigPaths({ projects: ["./tsconfig.json"]}),
 		typesPlugin(),
 	],
@@ -62,12 +48,4 @@ export default ({ mode }: { mode: string }) => defineConfig({
 	test: {
 		cache: process.env.CI ? false : undefined,
 	},
-	// resolve: {
-	// 	alias: [
-	// 		// absolute path needed because of https://github.com/vitest-dev/vitest/issues/2425
-	// 		// for tests only
-	// 		{ find: /^@\/(.*)/, replacement: `${path.resolve("src")}/$1/index.ts` },
-	// 		{ find: /^@tests\/(.*)/, replacement: `${path.resolve("tests")}/$1` },
-	// 	],
-	// },
 })
