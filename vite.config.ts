672ddd5e31c64933d0c4bdb7665b5365201bdead
Alan North <alanscodelog@gmail.com>
2023-07-19 17:38:51 -0300

    deps: updated template
    - fixes non-externalized deps problem if we ever need deps in the future

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,37 @@
 import glob from "fast-glob"
-import { builtinModules } from "module"
 import path from "path"
-import url from "url"
-import type { PluginOption } from "vite"
+import dts from "vite-plugin-dts"
+import { externalizeDeps } from "vite-plugin-externalize-deps"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
-import packageJson from "./package.json"
-import { run } from "./src/node_utils/run.js"
-
-
-const typesPlugin = (): PluginOption => ({
-	name: "typesPlugin",
-	// eslint-disable-next-line no-console
-	writeBundle: async () => run("npm run build:types").promise.catch(e => console.log(e)).then(() => undefined),
-})
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 // https://vitejs.dev/config/
 export default async ({ mode }: { mode: string }) => defineConfig({
 	plugins: [
-		tsconfigPaths({ projects: ["./tsconfig.json"]}),
-		typesPlugin(),
+		// it isn't enough to just pass the deps list to rollup.external since it will not exclude subpath exports
+		externalizeDeps(),
+		// even if we don't use aliases, this is needed to get imports based on baseUrl working
+		tsconfigPaths(),
+		dts({
+			entryRoot: "src",
+			tsconfigPath: "tsconfig.types.json",
+		}),
 	],
 	build: {
 		outDir: "dist",
-		cssCodeSplit: true,
 		lib: {
 			entry: glob.sync(path.resolve(__dirname, "src/**/*.ts")),
 			formats: ["es"],
+		
 		},
 		rollupOptions: {
-			external: [...builtinModules, ...Object.keys((packageJson as any).dependencies ?? {}), ...Object.keys((packageJson as any).peerDependencies ?? {}), /@babel\/runtime/],
 			output: {
 				preserveModulesRoot: "src",
 				preserveModules: true,
 			},
 		},
 		...(mode === "production" ? {
-			minify: false,
+			minify: true,
 		} : {
 			minify: false,
 			sourcemap: "inline",
@@ -47,5 +39,20 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 	},
 	test: {
 		cache: process.env.CI ? false : undefined,
+	},
+	resolve: {
+		alias: [
+			// for tests only, absolute path needed because of https://github.com/vitest-dev/vitest/issues/2425
+			{ find: /^@\/(.*)/, replacement: `${path.resolve("src")}/$1/index.ts` },
+			{ find: /^@tests\/(.*)/, replacement: `${path.resolve("tests")}/$1` },
+		],
+	},
+	server: {
+		watch: {
+			// for pnpm
+			followSymlinks: true,
+			// watch changes in linked repos
+			ignored: ["!**/node_modules/@alanscodelog/**"],
+		},
 	},
 })
