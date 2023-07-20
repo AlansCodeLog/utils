7a2c998b15dd13d3925c3df470e3d9ba894049fb
Alan North <alanscodelog@gmail.com>
2023-07-19 21:17:07 -0300

    fix: fix unresolved type aliases
    it seems we stil need the custom types plugin since dts is not compatible with tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import glob from "fast-glob"
 import path from "path"
+import url from "url"
+import type { PluginOption } from "vite"
 import dts from "vite-plugin-dts"
 import { externalizeDeps } from "vite-plugin-externalize-deps"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
+import { run } from "./src/node_utils/run.js"
+
+
+const typesPlugin = ({ fixOnly }: { fixOnly: boolean }): PluginOption => ({
+	name: "typesPlugin",
+	// eslint-disable-next-line no-console
+	writeBundle: async () => run(`npm run build:types${fixOnly ? ":fix" : ""}`).promise.catch(e => console.log(e)).then(() => undefined),
+})
+
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
 // https://vitejs.dev/config/
 export default async ({ mode }: { mode: string }) => defineConfig({
 	plugins: [
@@ -12,17 +26,21 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 		externalizeDeps(),
 		// even if we don't use aliases, this is needed to get imports based on baseUrl working
 		tsconfigPaths(),
+		// generates types for ts/vue files
+		// if only using ts, can be removed and build:types script used instead
 		dts({
 			entryRoot: "src",
-			tsconfigPath: "tsconfig.types.json",
+			tsconfigPath: "./tsconfig.types.json",
 		}),
+		// fixes alias and baseUrl imports in the generated types
+		// cannot be removed because dts is not compatible with tsconfigPaths
+		typesPlugin({ fixOnly: true }),
 	],
 	build: {
 		outDir: "dist",
 		lib: {
 			entry: glob.sync(path.resolve(__dirname, "src/**/*.ts")),
 			formats: ["es"],
-		
 		},
 		rollupOptions: {
 			output: {
