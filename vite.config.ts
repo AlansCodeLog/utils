e3571bbbe4013f817f1ee3070398b79f063c1065
Alan North <alanscodelog@gmail.com>
2023-07-20 21:20:16 -0300

    refactor: changed back to old way of building types
    Given the new way still required the types fix script because not all the vite plugins
    are compatible, it was too complicated.
    
    I think keeping it to simple build scripts that can be called on their on is better and also
    doesn't hide what's happening (tsc/vue-tsc for generation + tsc-alias to fix the types).

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import glob from "fast-glob"
 import path from "path"
 import url from "url"
 import type { PluginOption } from "vite"
-import dts from "vite-plugin-dts"
 import { externalizeDeps } from "vite-plugin-externalize-deps"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
@@ -10,12 +9,11 @@ import { defineConfig } from "vitest/config"
 import { run } from "./src/node_utils/run.js"
 
 
-const typesPlugin = ({ fixOnly }: { fixOnly: boolean }): PluginOption => ({
+const typesPlugin = (): PluginOption => ({
 	name: "typesPlugin",
 	// eslint-disable-next-line no-console
-	writeBundle: async () => run(`npm run build:types${fixOnly ? ":fix" : ""}`).promise.catch(e => console.log(e)).then(() => undefined),
+	writeBundle: async () => run(`npm run build:types`).promise.catch(e => console.log(e)).then(() => undefined),
 })
-
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
@@ -26,15 +24,8 @@ export default async ({ mode }: { mode: string }) => defineConfig({
 		externalizeDeps(),
 		// even if we don't use aliases, this is needed to get imports based on baseUrl working
 		tsconfigPaths(),
-		// generates types for ts/vue files
-		// if only using ts, can be removed and build:types script used instead
-		dts({
-			entryRoot: "src",
-			tsconfigPath: "./tsconfig.types.json",
-		}),
-		// fixes alias and baseUrl imports in the generated types
-		// cannot be removed because dts is not compatible with tsconfigPaths
-		typesPlugin({ fixOnly: true }),
+		// runs build:types script which takes care of generating types and fixing type aliases and baseUrl imports
+		typesPlugin(),
 	],
 	build: {
 		outDir: "dist",
