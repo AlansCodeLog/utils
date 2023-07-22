17b60f8f29d31fa4e65253fe1c3974a23abe3413
Alan North <alanscodelog@gmail.com>
2023-07-21 21:16:42 -0300

    build: updated to improved vite types plugin script

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import { run } from "./src/node_utils/run.js"
 const typesPlugin = (): PluginOption => ({
 	name: "typesPlugin",
 	// eslint-disable-next-line no-console
-	writeBundle: async () => run(`npm run build:types`).promise.catch(e => console.log(e)).then(() => undefined),
+	writeBundle: async () => run(`npm run build:types`).promise.catch(e => { console.log(e.stdout); process.exit(1) }).then(() => undefined),
 })
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
