76d0974600f9d5cb26298563a3fa21b90c6ebe1c
Alan North <alanscodelog@gmail.com>
2023-10-21 15:05:47 -0300

    feat: added TypedError class for easily creating typed error classes

---
+++ src/utils/TypedError.ts
@@ -0,0 +1,47 @@
+/**
+ * Base class for creating a strongly typed error class.
+ *
+ * This should not be used as is, instead you should extend from it like so with your own error types:
+ *
+ * ```ts
+ * // error types should look something like this
+ *	enum ERROR {
+ *		SOME_ERROR = "SOME_ERROR"
+ *	}
+ *	type ALL_ERRORS = ERROR // | SOME_OTHER_ERROR_CATEGORY
+ *
+ *	type ERROR_INFO = {
+ *		[ERROR.SOME_ERROR]: {
+ *			"required": "data"
+ *		}
+ *	}
+ *
+ *	export class KnownError<
+ *		T extends ALL_ERRORS,
+ *		TInfo extends ERROR_INFO[T] = ERROR_INFO[T],
+ *	> extends TypedError<T, TInfo> {}
+ *
+ * // now the contructor is typed
+ * const error = new KnownError(ERROR.SOME_ERROR, "message", {
+ * 	// type error if missing properties
+ * })
+ * ```
+ */
+export class TypedError<
+	T extends string,
+	TErrorInfo = any,
+> extends Error {
+	code: T
+
+	info: TErrorInfo
+
+	constructor(
+		code: T,
+		str: string,
+		info: TErrorInfo,
+	) {
+		super(str)
+		this.code = code
+		this.info = info
+	}
+}
---
+++ tests/utils/TypedError.spec.ts
@@ -0,0 +1,33 @@
+import { expectType, testName, TypedError } from "index.js"
+import { describe, expect, it } from "vitest"
+
+
+describe(testName(), () => {
+	it("works", () => {
+		enum ERROR {
+			SOME_ERROR = "SOME_ERROR",
+		}
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		type ALL_ERRORS = ERROR
+
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		type ERROR_INFO = {
+			[ERROR.SOME_ERROR]: {
+				"required": "data"
+			}
+		}
+
+		class MyKnownError<
+			T extends ALL_ERRORS,
+			TInfo extends ERROR_INFO[T] = ERROR_INFO[T],
+		> extends TypedError<T, TInfo> {
+
+		}
+
+		// @ts-expect-error check it's detecting missing type of error info
+		const error = new MyKnownError(ERROR.SOME_ERROR, "message", {})
+		const validError = new MyKnownError(ERROR.SOME_ERROR, "message", { required: "data" })
+
+		expectType<typeof validError, "===", MyKnownError<ERROR.SOME_ERROR>>(true)
+	})
+})
