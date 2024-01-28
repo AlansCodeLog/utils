edc28ebb3563212cd04c4178771fe90b56b85e11
Alan North <alanscodelog@gmail.com>
2024-01-28 11:06:51 -0300

    deps: updated deps + node version
    BREAKING CHANGE: Minimum node version is now 20.

--- src/utils/evalTemplateString.ts
+++ src/utils/evalTemplateString.ts
@@ -10,7 +10,6 @@ export function evalTemplateString(template: TemplateStringsArray, substitutions
 		const str = template[i]
 
 		// we're relying on the coercion to correctly transform the variables like a template string would
-		// eslint-disable-next-line @typescript-eslint/restrict-plus-operands
 		res += str + (i < substitutions.length ? substitutions[i] : "")
 	}
 	return res
