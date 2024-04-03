5dc08468cf23e37cbf53bc1a4428ec28fa1f635a
Alan North <alanscodelog@gmail.com>
2024-04-03 16:08:25 -0300

    deps: updated deps

--- src/utils/omit.ts
+++ src/utils/omit.ts
@@ -2,7 +2,13 @@ import { keys as objectKeys } from "./keys.js"
 /**
  * Returns a copy of the object without the specified properties.
  */
-export function omit<T extends {}, TKeys extends (keyof T)[]>(obj: T, keys: TKeys): Omit<T, TKeys[number]> {
+export function omit<
+	T extends Record<any, any>,
+	TKeys extends (keyof T)[],
+>(
+	obj: T,
+	keys: TKeys
+): Omit<T, TKeys[number]> {
 	const copy: any = {}
 	for (const key of objectKeys(obj)) {
 		if (!keys.includes(key)) {
