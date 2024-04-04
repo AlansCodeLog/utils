8ec746c731689f445278619aa0ea40477e524b36
Alan North <alanscodelog@gmail.com>
2024-04-03 22:51:43 -0300

    feat: added ability to pass initial array to RingBuffer

--- src/utils/RingBuffer.ts
+++ src/utils/RingBuffer.ts
@@ -4,11 +4,15 @@
  * Type must be specified at construction, otherwise we can't know the type of items and it will be unknown[].
  */
 export class RingBuffer<T> {
-	items: T[] = [] as T[]
-
-	constructor(public readonly size: number = 100) {
+	constructor(
+		public readonly size: number = 100,
+		public items: T[] = [] as T[]
+	) {
 		if (size <= 0) {
 			throw new Error("Size must be greater than 0")
+		}
+		if (items.length > size) {
+			throw new Error("Initial array cannot be larger than size.")
 		}
 	}
 
