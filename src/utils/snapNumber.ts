1ee02fb82d6adb8df1300c0eeca6c33a9d529a96
Alan North <alanscodelog@gmail.com>
2024-11-13 14:42:01 -0300

    feat: added ceil/floor options to snapNumber

--- src/utils/snapNumber.ts
+++ src/utils/snapNumber.ts
@@ -1,6 +1,8 @@
 /**
  * The classic `Math.round(num / step) * step` for snapping numbers to a certain step.
+ *
+ * Can optionally round up ("ceil") or down ("floor").
  */
-export function snapNumber(num: number, step: number): number {
-	return Math.round(num / step) * step
+export function snapNumber(num: number, step: number, dir: "floor" | "ceil" | "round" = "round"): number {
+	return Math[dir](num / step) * step
 }
--- tests/utils/snapNumber.spec.ts
+++ tests/utils/snapNumber.spec.ts
@@ -6,4 +6,6 @@ import { snapNumber } from "../../src/utils/snapNumber.js"
 it("snap", () => {
 	expect(snapNumber(0.1, 10)).to.equal(0)
 	expect(snapNumber(5.1, 10)).to.equal(10)
+	expect(snapNumber(0.1, 10, "ceil")).to.equal(10)
+	expect(snapNumber(9.9, 10, "floor")).to.equal(0)
 })
