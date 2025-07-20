16ceb0082a6518bfd068216db38da231084fcb33
Alan North <alanscodelog@gmail.com>
2025-07-20 18:49:26 -0300

    feat: added prefixMessage param to unreachable
    BREAKING CHANGE: The new prefixMessage param is first in the unreachable
    function, the old param to override the entire message is now second.

--- src/utils/unreachable.ts
+++ src/utils/unreachable.ts
@@ -1,8 +1,16 @@
 /**
  * To indicate the code path is unreachable.
  *
+ * @param prefixMessage Prefix the default message instead. It will be joined like `${prefixMessage}\n${message}`
+ *
  * @param message Override the default message "This error should never happen, please file a bug report."
  */
-export function unreachable(message: string = `This error should never happen, please file a bug report.`): never {
+export function unreachable(
+	prefixMessage?: string,
+	message: string = `This error should never happen, please file a bug report.`
+): never {
+	if (prefixMessage) {
+		message = `${prefixMessage}\n${message}`
+	}
 	throw new Error(message)
 }
