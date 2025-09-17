1b655392f468b4c2d3efb250cfde7f36ad152739
Alan North <alanscodelog@gmail.com>
2025-09-17 19:28:26 -0300

    fix: fixed eslint + build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from "@alanscodelog/vite-config"
-
 // https://vitejs.dev/config/
-export default defineConfig()
+export default defineConfig({
+	entryGlobs: [
+		"src/**/*.ts",
+		"!*.ts",
+		"!*.js",
+	],
+	pluginOpts: {
+		typesPlugin: {
+			dtsGenerator: "tsc",
+			project: "tsconfig.types.json",
+		}
+	}
+}, {},)
